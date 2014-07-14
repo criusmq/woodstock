e5fef1e42d49a9b841a184338a26efc55c59a49e
Pierre-Alexandre St-Jean <pa@stjean.me>
2014-07-14 11:01:22 -0400

    go fmt and test multiplicity in simple.spept

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	// "github.com/criusmq/woodstock/graph"
 	// "github.com/criusmq/woodstock/mutator"
-  // "github.com/gorilla/websocket"
-  // "github.com/gorilla/mux"
-  
-  // "github.com/codegangsta/negroni" - not sure this is needed but there are 
-  // some usefull stuff like static file serving
- 
-  // "html"
-  // "net/http"
-  // "time"
+	// "github.com/gorilla/websocket"
+	// "github.com/gorilla/mux"
+
+	// "github.com/codegangsta/negroni" - not sure this is needed but there are
+	// some usefull stuff like static file serving
+
+	// "html"
+	// "net/http"
+	// "time"
 	"fmt"
 )
+
 func main() {
 
 	fmt.Println("Hello")
