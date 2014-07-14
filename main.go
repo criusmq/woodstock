692286be486e26b9715e195a09860976254ce400
Pierre-Alexandre St-Jean <pa@stjean.me>
2014-07-14 10:36:48 -0400

    Just some reordering
    anyways go fmt is gonna do it

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	// "github.com/criusmq/woodstock/graph"
 	// "github.com/criusmq/woodstock/mutator"
   // "github.com/gorilla/websocket"
-	// "github.com/codegangsta/negroni"
   // "github.com/gorilla/mux"
+  
+  // "github.com/codegangsta/negroni" - not sure this is needed but there are 
+  // some usefull stuff like static file serving
+ 
   // "html"
   // "net/http"
   // "time"
